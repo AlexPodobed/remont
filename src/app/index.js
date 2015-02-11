992711602620348d4a464c77a2f7f79d8a0910ac
Oleksii_Podobed@epam.com <alexey.podobed@gmail.com>
2015-02-11 18:01:47 +0100

    add bootstrap.ui

--- src/app/index.js
+++ src/app/index.js
@@ -1,42 +1,44 @@
 'use strict';
 
 angular.module('remont', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router'])
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: 'app/home/home.html',
-                controller: 'HomeCtrl'
-            })
-            .state('contacts', {
-                url: '/contacts',
-                templateUrl: 'app/contacts/contacts.html',
-                controller: 'ContactsCtrl'
-            })
-            .state('portfolio', {
-                url: '/our_works',
-                templateUrl: 'app/portfolio/portfolio.html',
-                controller: 'PortfolioCtrl'
-            })
-            .state('workPreview', {
-                url: '/our_works/:id',
-                templateUrl: 'app/workPreview/workpreview.html',
-                controller: 'WorkPreviewCtrl'
-            });
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      })
+      .state('contacts', {
+        url: '/contacts',
+        templateUrl: 'app/contacts/contacts.html',
+        controller: 'ContactsCtrl'
+      })
+      .state('portfolio', {
+        url: '/our_works',
+        templateUrl: 'app/portfolio/portfolio.html',
+        controller: 'PortfolioCtrl'
+      })
+      .state('workPreview', {
+        url: '/our_works/:id',
+        templateUrl: 'app/workPreview/workpreview.html',
+        controller: 'WorkPreviewCtrl'
+      });
 
-        $urlRouterProvider.otherwise('/home');
-    })
-    .controller('GlobalCtrl', ['$scope','$window', function($scope, $window){
+    $urlRouterProvider.otherwise('/home');
+  })
+  .controller('GlobalCtrl', ['$scope', '$window', function ($scope, $window) {
     console.log($window.innerHeight)
-        $scope.height =  $window.innerHeight;
-    }])
-    .run(function($rootScope, $window){
-        $rootScope.$on('$stateChangeSuccess', function(e, toState, toParams, fromState){
-            if(fromState.name){
-                console.log(1)
+    $scope.height = $window.innerHeight;
+  }])
+  .run(function ($rootScope, $window) {
+    $rootScope.$on('$stateChangeSuccess', function (e, toState, toParams, fromState) {
+      if (fromState.name) {
+        console.log(1)
 //                $window.scrollTo(0,1)
-                setTimeout(function(){$window.scrollTo(0,1);}, 0)
-            }
-        })
+        setTimeout(function () {
+          $window.scrollTo(0, 1);
+        }, 0)
+      }
     })
+  })
 ;
--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('remont')
-  .controller('NavbarCtrl', function ($scope) {
+  .controller('NavbarCtrl', function ($scope, $window) {
     $scope.date = new Date();
+
+    $scope.fixedPoint = ($window.innerWidth <= 767) ? 10 : 80;
   })
   .directive('fixedMenu', ['$window', function($window){
     function checkPosition(el, attr){
-      console.log($window.scrollY)
       if($window.scrollY >= +attr.fixedMenu){
         $(el).addClass('fixed');
-
       }else{
         $(el).removeClass('fixed');
       }
@@ -18,12 +18,41 @@ angular.module('remont')
     return {
       restrict: 'A',
       link: function(scope, el, attr){
-        console.log(attr.fixedMenu)
 
         $($window).on('scroll', function(){
           checkPosition(el, attr);
         });
         checkPosition(el, attr);
+      }
+    }
+  }])
+  .directive('checkResize', ['$window', function($window){
+    function debounce(func, wait, immediate) {
+      var timeout;
+      return function () {
+        var context = this, args = arguments;
+        var later = function () {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+    function checkAndSetModel(scope){
+      scope.ngModel = ($window.innerWidth <= 767) ? 10 : 80;
+      if(!scope.$$phase){ scope.$apply() }
+    }
+
+    return {
+      scope: {
+        ngModel: "="
+      },
+      link: function(scope, element, attr){
+        var applyFn = debounce(function(){checkAndSetModel(scope)}, 300);
+        $($window).on('resize', applyFn);
       }
     }
   }]);
